24bc2702cabf351f0885cc4eec2ae2f502253f6d
FabioLapiana <131192770+FabioLapiana@users.noreply.github.com>
2024-03-24 23:10:26 +0100

    add canvas toolbar

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -33,7 +33,7 @@ export const OrgSidebar = () => {
                         "font-semibold text-2xl",
                         font.className,
                     )}>
-                        Miro
+                        Firo
                     </span>
                 </div>
             </Link>
--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,6 +1,17 @@
 "use client";
 
 import { useSelf } from "@/liveblocks.config";
+import { useCallback, useMemo, useState, useEffect } from "react";
+import { 
+    Camera, 
+    CanvasMode, 
+    CanvasState, 
+    Color,
+    LayerType,
+    Point,
+    Side,
+    XYWH,
+  } from "@/types/canvas";
 
 import { Info } from "./info";
 import { Participants } from "./participants";
@@ -11,12 +22,22 @@ interface CanvasProps {
   };
 
 export const Canvas = ({boardId, } : CanvasProps ) => {
+    const [canvasState, setCanvasState] = useState<CanvasState>({
+        mode: CanvasMode.None,
+    });
     const info = useSelf((me) => me.info);
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
             <Info boardId={boardId}/>
             <Participants />
-            <Toolbar />
+            <Toolbar
+                canvasState={canvasState}
+                setCanvasState={setCanvasState}
+                canRedo={false}
+                canUndo={false}
+                undo={()=>{}}
+                redo={()=>{}}
+            />
         </main>
     )
 }
