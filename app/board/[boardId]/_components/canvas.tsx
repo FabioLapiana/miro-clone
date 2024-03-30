c3f2314077c844a2c4ff6139daf12abfc136e9e5
FabioLapiana <131192770+FabioLapiana@users.noreply.github.com>
2024-03-30 16:48:07 +0100

    canvas state

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -25,6 +25,7 @@ import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
 import { CursorsPresence } from "./cursors-presence";
+import { pointerEventToCanvasPoint } from "@/lib/utils";
 
 interface CanvasProps {
     boardId: string;
@@ -36,9 +37,50 @@ export const Canvas = ({boardId, } : CanvasProps ) => {
     });
     const info = useSelf((me) => me.info);
 
+    const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
+
     const history = useHistory();
     const canUndo = useCanUndo();
     const canRedo = useCanRedo();
+
+    const onWheel = useCallback((e: React.WheelEvent) => {
+        setCamera((camera) => ({
+          x: camera.x - e.deltaX,
+          y: camera.y - e.deltaY,
+        }));
+      }, []);
+
+    const onPointerMove = useMutation((
+        { setMyPresence }, 
+        e: React.PointerEvent
+      ) => {
+        e.preventDefault();
+
+        const current = pointerEventToCanvasPoint(e, camera);
+    
+        // if (canvasState.mode === CanvasMode.Pressing) {
+        //   startMultiSelection(current, canvasState.origin);
+        // } else if (canvasState.mode === CanvasMode.SelectionNet) {
+        //   updateSelectionNet(current, canvasState.origin);
+        // } else if (canvasState.mode === CanvasMode.Translating) {
+        //   translateSelectedLayers(current);
+        // } else if (canvasState.mode === CanvasMode.Resizing) {
+        //   resizeSelectedLayer(current);
+        // } else if (canvasState.mode === CanvasMode.Pencil) {
+        //   continueDrawing(current, e);
+        // }
+    
+        setMyPresence({ cursor: current });
+      }, 
+      [
+        // continueDrawing,
+        // camera,
+        // canvasState,
+        // resizeSelectedLayer,
+        // translateSelectedLayers,
+        // startMultiSelection,
+        // updateSelectionNet,
+      ]);
 
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
@@ -54,6 +96,8 @@ export const Canvas = ({boardId, } : CanvasProps ) => {
             />
             <svg
                 className="h-[100vh] w-[100vw]"
+                onWheel={onWheel}
+                onPointerMove={onPointerMove}
             >
                 <g
                 ><CursorsPresence />
